72936c58a63667f26aff4bdea28c330d875ae896
Matthew Sanders <mattomatic@gmail.com>
2013-06-13 09:31:44 -0500

    Formatting changes.

--- graph/loader.go
+++ graph/loader.go
@@ -2,36 +2,36 @@ package graph
 
 import (
 	"bufio"
-	"os"
 	"fmt"
+	"os"
 )
 
 func LoadGraph(filename string) *Graph {
-    fp, _ := os.Open(filename)
-    reader := bufio.NewReader(fp)
-    g := NewGraph()    
-    readHeader(reader)
-    var src, dst int
+	fp, _ := os.Open(filename)
+	reader := bufio.NewReader(fp)
+	g := NewGraph()
+	readHeader(reader)
+	var src, dst int
 
-    for scanLine(reader, &src, &dst) {
-        srcNode := getOrCreate(g, src)
-        dstNode := getOrCreate(g, dst)
-        srcNode.AddEdges(dstNode)
-    }
-    
-    return g
+	for scanLine(reader, &src, &dst) {
+		srcNode := getOrCreate(g, src)
+		dstNode := getOrCreate(g, dst)
+		srcNode.AddEdges(dstNode)
+	}
+
+	return g
 }
 
-func scanLine(reader *bufio.Reader, src *int, dst *int) (bool) {
-    n, err := fmt.Fscanf(reader, "%d %d\n", src, dst)
-    return n == 2 && err == nil
+func scanLine(reader *bufio.Reader, src *int, dst *int) bool {
+	n, err := fmt.Fscanf(reader, "%d %d\n", src, dst)
+	return n == 2 && err == nil
 }
 
 func readHeader(reader *bufio.Reader) {
-    b, _ := reader.Peek(1)
-    if b[0] == '#' {
-        reader.ReadLine()
-    }
+	b, _ := reader.Peek(1)
+	if b[0] == '#' {
+		reader.ReadLine()
+	}
 }
 
 func getOrCreate(g *Graph, id int) *Node {
